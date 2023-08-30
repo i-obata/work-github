04bae338bcc3a9e84fe7237bc14aaffe79166bb6
EC2 Default User <ec2-user@ip-172-31-19-78.ap-northeast-1.compute.internal>
2023-08-30 12:46:36 +0000

    商品新規登録画面

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    
+    # itemsコントローラー
+    resources :items, only:[:new, :create, :index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
