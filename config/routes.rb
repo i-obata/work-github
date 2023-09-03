770eeeda9174e9867d8dd84342bc8baf066dac9b
EC2 Default User <ec2-user@ip-172-31-19-78.ap-northeast-1.compute.internal>
2023-09-03 14:14:50 +0000

    会員情報

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,10 @@ Rails.application.routes.draw do
     
     # itemsコントローラー
     resources :items, only:[:new, :create, :index, :show, :edit, :update]
+    
+    # customersコントローラー
+    resources :customers, only:[:index, :show, :edit, :update]
+
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
